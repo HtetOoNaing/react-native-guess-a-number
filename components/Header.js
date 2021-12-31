51a5707f0efdf1d573d03681ddae99968d3932ba
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-12-31 11:01:34 +0630

    update header style

--- components/Header.js
+++ components/Header.js
@@ -15,9 +15,10 @@ const styles = StyleSheet.create({
 	header: {
 		width: '100%',
 		height: 90,
-		paddingTop: 36,
+		paddingTop: 32,
 		backgroundColor: '#f7287b',
-		alignItems: 'center'
+		alignItems: 'center',
+		justifyContent: 'center'
 	},
 	headerTitle: {
 		color: 'black',
