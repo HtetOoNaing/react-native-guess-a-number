1f6a7a7cce55059c7ffeb88a110591b597a79468
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-04 12:11:34 +0630

    add game over screen and check the win condition

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Alert, Button, StyleSheet, Text, View } from 'react-native'
 import Card from '../components/Card';
 import NumberContainer from '../components/NumberContainer';
@@ -14,10 +14,17 @@ const generateRandomBetween = (min, max, exclude) => {
 	}
 }
 
-const GameScreen = ({ userChoice }) => {
+const GameScreen = ({ userChoice, onGameOver }) => {
 	const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userChoice))
+	const [rounds, setRounds] = useState(0)
 	const currentLow = useRef(1)
 	const currentHigh = useRef(100)
+
+	useEffect(() => {
+		if(currentGuess === userChoice) {
+			onGameOver(rounds)
+		}
+	}, [currentGuess, userChoice, onGameOver])
 
 	const nextGuessHandler = direction => {
 		if((direction === 'lower' && currentGuess < userChoice) || (direction === 'greater' && currentGuess > userChoice)) {
@@ -33,6 +40,7 @@ const GameScreen = ({ userChoice }) => {
 		}
 		const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
 		setCurrentGuess(nextNumber)
+		setRounds(currentRound => currentRound + 1)
 	}
 	return (
 		<View style={styles.screen}>
