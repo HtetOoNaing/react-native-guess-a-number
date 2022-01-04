c5bc89072948f324b3ead4197bb13483980406a5
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-04 12:16:04 +0630

    add roundNumber and userNumber on gameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const GameOverScreen = () => {
+const GameOverScreen = ({ roundNumber, userNumber  }) => {
 	return (
 		<View style={styles.screen}>
 			<Text>The game is over!</Text>
+			<Text>Number of rounds {roundNumber}</Text>
+			<Text>Number was {userNumber}</Text>
 		</View>
 	)
 }
