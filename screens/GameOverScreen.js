a5830726686781095e5674e7e80fa49eec5d4e1d
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-30 16:17:54 +0630

    show image with link

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
-import BodyText from '../components/BodyText'
-import TitleText from '../components/TitleText'
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
-const GameOverScreen = ({ roundNumber, userNumber, onNewGame  }) => {
+const GameOverScreen = ({ roundNumber, userNumber, onNewGame }) => {
 	return (
 		<View style={styles.screen}>
 			<TitleText>The game is over!</TitleText>
-			<Image source={require('../assets/brazil.png')} style={styles.image} resizeMode="contain" />
+			<Image
+				// source={require("../assets/brazil.png")}
+				source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/800px-Flag_of_the_Netherlands.svg.png"}}
+				style={styles.image}
+				resizeMode="contain"
+			/>
 			<BodyText style={styles.text}>Number of rounds {roundNumber}</BodyText>
 			<BodyText style={styles.text}>Number was {userNumber}</BodyText>
-			<Button title='NEW GAME' onPress={onNewGame} /> 
+			<Button title="NEW GAME" onPress={onNewGame} />
 		</View>
-	)
-}
+	);
+};
 
-export default GameOverScreen
+export default GameOverScreen;
 
 const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+		justifyContent: "center",
+		alignItems: "center",
 	},
 	image: {
-		width: '80%',
+		width: "80%",
 		height: 300,
 	},
 	text: {
 		fontSize: 20,
-		marginBottom: 10
-	}
-})
+		marginBottom: 10,
+	},
+});
