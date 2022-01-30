5c63bf14be31e4cac0d933843dbeef8574c8af33
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-30 17:45:14 +0630

    using icons for lower and greater

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,74 +1,88 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Alert, Button, StyleSheet, Text, View } from 'react-native'
-import Card from '../components/Card';
-import MainButton from '../components/MainButton';
-import NumberContainer from '../components/NumberContainer';
-import DefaultStyles from '../constants/default-styles';
+import React, { useEffect, useRef, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Card from "../components/Card";
+import MainButton from "../components/MainButton";
+import NumberContainer from "../components/NumberContainer";
+import DefaultStyles from "../constants/default-styles";
 
 const generateRandomBetween = (min, max, exclude) => {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	const rndNum = Math.floor(Math.random() * (max - min)) + min;
-	if(rndNum === exclude) {
-		return generateRandomBetween(min, max, exclude)
+	if (rndNum === exclude) {
+		return generateRandomBetween(min, max, exclude);
 	} else {
-		return rndNum
+		return rndNum;
 	}
-}
+};
 
 const GameScreen = ({ userChoice, onGameOver }) => {
-	const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userChoice))
-	const [rounds, setRounds] = useState(0)
-	const currentLow = useRef(1)
-	const currentHigh = useRef(100)
+	const [currentGuess, setCurrentGuess] = useState(
+		generateRandomBetween(1, 100, userChoice)
+	);
+	const [rounds, setRounds] = useState(0);
+	const currentLow = useRef(1);
+	const currentHigh = useRef(100);
 
 	useEffect(() => {
-		if(currentGuess === userChoice) {
-			onGameOver(rounds)
+		if (currentGuess === userChoice) {
+			onGameOver(rounds);
 		}
-	}, [currentGuess, userChoice, onGameOver])
+	}, [currentGuess, userChoice, onGameOver]);
 
-	const nextGuessHandler = direction => {
-		if((direction === 'lower' && currentGuess < userChoice) || (direction === 'greater' && currentGuess > userChoice)) {
-			Alert.alert('Don\'t lie!', 'You know that this is wrong...', [
-				{ text: 'Sorry!', style: 'cancel'}
+	const nextGuessHandler = (direction) => {
+		if (
+			(direction === "lower" && currentGuess < userChoice) ||
+			(direction === "greater" && currentGuess > userChoice)
+		) {
+			Alert.alert("Don't lie!", "You know that this is wrong...", [
+				{ text: "Sorry!", style: "cancel" },
 			]);
 			return;
 		}
-		if(direction === 'lower') {
-			currentHigh.current = currentGuess 
+		if (direction === "lower") {
+			currentHigh.current = currentGuess;
 		} else {
-			currentLow.current = currentGuess 
+			currentLow.current = currentGuess;
 		}
-		const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
-		setCurrentGuess(nextNumber)
-		setRounds(currentRound => currentRound + 1)
-	}
+		const nextNumber = generateRandomBetween(
+			currentLow.current,
+			currentHigh.current,
+			currentGuess
+		);
+		setCurrentGuess(nextNumber);
+		setRounds((currentRound) => currentRound + 1);
+	};
 	return (
 		<View style={styles.screen}>
 			<Text style={DefaultStyles.title}>Opponent's Guesses</Text>
 			<NumberContainer>{currentGuess}</NumberContainer>
 			<Card style={styles.buttonContainer}>
-				<MainButton onPress={() => nextGuessHandler('lower')}>LOWER</MainButton>
-				<MainButton onPress={() => nextGuessHandler('greater')}>GREATER</MainButton>
+				<MainButton onPress={() => nextGuessHandler("lower")}>
+					<Ionicons name="md-remove" size={24} color="white" />
+				</MainButton>
+				<MainButton onPress={() => nextGuessHandler("greater")}>
+					<Ionicons name="md-add" size={24} color="white" />
+				</MainButton>
 			</Card>
 		</View>
-	)
-}
+	);
+};
 
-export default GameScreen
+export default GameScreen;
 
 const styles = StyleSheet.create({
 	screen: {
 		flex: 1,
-		padding: 10, 
-		alignItems: 'center'
+		padding: 10,
+		alignItems: "center",
 	},
 	buttonContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
+		flexDirection: "row",
+		justifyContent: "space-around",
 		marginTop: 20,
 		width: 300,
-		maxWidth: '90%'
-	}
-})
+		maxWidth: "90%",
+	},
+});
