5916b18cb00af7c3c2d12753577ea3e37905e7db
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-01-30 16:31:20 +0630

    update game over screen text

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Image, StyleSheet, Text, View } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import colors from "../constants/colors";
 
 const GameOverScreen = ({ roundNumber, userNumber, onNewGame }) => {
 	return (
@@ -9,12 +10,19 @@ const GameOverScreen = ({ roundNumber, userNumber, onNewGame }) => {
 			<TitleText>The game is over!</TitleText>
 			<Image
 				// source={require("../assets/brazil.png")}
-				source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/800px-Flag_of_the_Netherlands.svg.png"}}
+				source={{
+					uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/800px-Flag_of_the_Netherlands.svg.png",
+				}}
 				style={styles.image}
 				resizeMode="contain"
 			/>
-			<BodyText style={styles.text}>Number of rounds {roundNumber}</BodyText>
-			<BodyText style={styles.text}>Number was {userNumber}</BodyText>
+			<View style={styles.resultContainer}>
+				<BodyText style={styles.text}>
+					Your phone needed <Text style={styles.highlight}>{roundNumber}</Text>{" "}
+					rounds to guess the number{" "}
+					<Text style={styles.highlight}>{userNumber}</Text>{" "}
+				</BodyText>
+			</View>
 			<Button title="NEW GAME" onPress={onNewGame} />
 		</View>
 	);
@@ -32,8 +40,15 @@ const styles = StyleSheet.create({
 		width: "80%",
 		height: 300,
 	},
+	resultContainer: {
+		marginHorizontal: 30
+	},
 	text: {
 		fontSize: 20,
-		marginBottom: 10,
+		marginBottom: 20,
+	},
+	highlight: {
+		color: colors.primary,
+		fontWeight: "bold"
 	},
 });
